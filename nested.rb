fb851291a85330d51065c2a44dea7310a0b24d32
Corina Talavera <corina.talavera@gmail.com>
2018-11-03 14:41:41 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for] << "GUI" 
+     changing_alan[:alan_kay][:known_for] << "GUI" 
      
      
 end
@@ -128,5 +128,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-adding_to_dennis[:dennis_ritchie][:languages][1]).to include("Assembly")
+  adding_to_dennis[:dennis_ritchie][:languages][1]).to include("Assembly")
 end
